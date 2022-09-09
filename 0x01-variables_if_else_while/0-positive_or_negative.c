c77de705d36e10399e2f806270cace8e509a47ea
Adunbpenuel <adunbpenuel@gmail.com>
2022-09-09 07:29:59 -0700

    if else

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <iostream>
+
+
+int main(void)
+	/*
+	 * cout<<"Random numbers generated between 0 and 1 :"<<end/2;
+	 * srand( (unsigned)time(0) );
+	 */
+{
+	int n;
+
+	srand(time(0));
+	n=rand() - RAND_MAX/2;
+	else
+		return (0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,2 @@
+#!/bin/bash
+C-variables, 1f, else, while
