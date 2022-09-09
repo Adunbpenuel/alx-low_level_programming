fa3733b9f355a4258e3b3f617c2a27588a4509dc
Adunbpenuel <adunbpenuel@gmail.com>
2022-09-09 14:43:02 -0700

    while

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,18 +1,37 @@
 #include <stdlib.h>
 #include <time.h>
-#include <iostream>
+#include <stdio.h>
 
-
+/**
+ * main - Prints if number is positive, zero or negative
+ *
+ * Return: Always (Success)
+ */
 int main(void)
-	/*
-	 * cout<<"Random numbers generated between 0 and 1 :"<<end/2;
-	 * srand( (unsigned)time(0) );
-	 */
+
 {
 	int n;
 
 	srand(time(0));
-	n=rand() - RAND_MAX/2;
-	else
-		return (0);
+  n = rand() - RAND_MAX / 2;
+  if (n > 0)
+}
+{
+	printf("%d is positive\n", n);
+
+}
+else if (n == 0)
+
+{
+	printf("%d is zero\n", n);
+
+}
+else
+{
+	printf("%d is negative\n", n);
+
+}
+{
+	return (0);
+
 }
