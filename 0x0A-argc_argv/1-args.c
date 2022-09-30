689f9a486326ac1a1258010a87de3d492f417cb4
Adunbpenuel <adunbpenuel@gmail.com>
2022-09-30 07:26:53 -0700

    hello Duniya

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - print a number, followed by a new line
+ * @argc: number of command line arguments.
+ * @argv: array that contains the program command line arguments.
+ * Return: 0 - successfully.
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
